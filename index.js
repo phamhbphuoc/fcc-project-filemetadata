f70e4c2d109564eddac7135d935d6b3743b7efde
Phuoc Pham <wetherzad97@yahoo.com.vn>
2025-01-13 10:01:07 +0000

    feat: added file metadata parser api

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+  const upfile = req.file;
 
+  const name = upfile.originalname;
+  const type = upfile.mimetype;
+  const size = upfile.size;
 
+  res.json({
+    name,
+    type,
+    size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
